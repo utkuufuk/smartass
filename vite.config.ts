e87842835d655cf1bce942686ebec91d219a8770
Utku Ufuk <utkuufuk@gmail.com>
2023-05-06 15:12:08 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: './docs',
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+})
